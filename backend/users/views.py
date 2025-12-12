3f3b1ff85e77220cd6639db56a51d7f64bf622e4
Walton Karl <100638896+WaltsCS@users.noreply.github.com>
2025-12-12 23:32:54 +0800

    Created both StudentProfile and AdminProfile, successful login and access auth tests

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,6 +2,13 @@ from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer, UserSerializer
 
+# Role-Restricted test endpoints =======
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import StudentProfile, AdminProfile
+# Role-Restricted test endpoints =======
+
 class RegisterAPI(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
@@ -12,3 +19,24 @@ class UserAPI(generics.RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
+    
+# Role-Restricted test endpoints =======
+class StudentOnlyAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        if not StudentProfile.objects.filter(user=request.user).exists():
+            return Response({"error": "You are not a student"}, status=403)
+
+        return Response({"message": "Welcome Student!"})
+
+
+class AdminOnlyAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        if not AdminProfile.objects.filter(user=request.user).exists():
+            return Response({"error": "You are not an admin"}, status=403)
+
+        return Response({"message": "Welcome Admin!"})
+# Role-Restricted test endpoints =======
